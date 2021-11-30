ea0c7cd3bfe9e7e6f08dc04efb25cfe4fdf7316a
romeo mureithi <romeomureithi@gmail.com>
2021-11-30 23:05:13 +0300

    Updated dice roller simulator

--- dice_roller.py
+++ dice_roller.py
@@ -10,36 +10,26 @@ def dice():
         '5': 25,
         '6': 30
     }
-    user_points = 0
-    comp_points = 0
+
     user = []
-    comp = []
-    turn = str(input("Press A to roll again, B to quit game: "))
-    if turn == 'A' or 'a':
-        try:
-            for play in range(1, 6):
-                user_turn = user.append(random.randint(1, 6))
-                comp_turn = comp.append(random.randint(1, 6))
-
-                for dice, points in dice_nums.items():
-                    for i in user:
-                        if str(i) == dice:
-                            user_points =+ points
-                    for j in comp:
-                        if str(j) == dice:
-                            comp_points =+ points
-
-                if user_points >= 100:
-                    print("You win")
-                elif comp_points >= 100:
-                    print("Computer wins")
-
-        except Exception:
+    user_points = 0
+    for steps in range(5, 0, -1):
+        turn = str(input("Press A to roll dice, B to quit game: "))
+        if turn == 'A' 'a':
+            scores = random.randint(1, 6)
+            print(f"You have {steps} remaining. Good Luck!")
+            print(f'You rolled a {scores}\n')
+            user.append(scores)
+        else:
             print("Exiting Game...")
-    else:
-        pass
-    return user_points, comp_points
+            sleep(1)
+            break
 
+    for diceFace, diceScore in dice_nums.items():
+        for faceRolled in range(len(user)):
+            if int(diceFace) == user[faceRolled]:
+                user_points += diceScore
+    print(f"Total Points: {user_points}")
 
 if __name__ == "__main__":
     play = str(input("Press Y to start game, N to leave game: "))
@@ -49,16 +39,9 @@ if __name__ == "__main__":
             sleep(1)
             print("Points Awarded on dice rolled:\n1 - 5 points\n2 - 10 points\n3 - 15 points\n4 - 20 points\n5 - 25 points\n6 - 30 points\n")
             sleep(1)
-            count = 0
-            while True:
-                if count <= 5:
-                    dice()
-                    count += 1
-                else:
-                    break
-                break
-        except Exception:
-            print("Invalid Input leaving game")
+            dice()
+        except Exception as error:
+            print(f"Error Detected: {error}\nInvalid. Aborting Game...")
     else:
         print("Leaving game...")
         sleep(.75)
